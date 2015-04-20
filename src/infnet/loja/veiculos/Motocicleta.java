a63969e82ffc99e8755171cf27a4c29c1051382e
VanessaEly <vah_ely@hotmail.com>
2015-04-20 15:37:20 -0300

    atribuindo variáveis à motocicleta

---
+++ src/infnet/loja/veiculos/Motocicleta.java
@@ -0,0 +1,13 @@
+package infnet.loja.veiculos
+
+public class Motocicleta 
+{
+	String chassi;
+	String montadora;
+	String modelo;
+	String tipo;
+	String cor;
+	int cilindrada;
+	int capacidadeDoTanque; 
+	float preço;
+}
