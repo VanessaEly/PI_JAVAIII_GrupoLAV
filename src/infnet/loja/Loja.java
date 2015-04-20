65bfe77ad1993d45bcbd6f2e637c16f21b0ec496
VanessaEly <vah_ely@hotmail.com>
2015-04-20 15:42:25 -0300

    atribuindo variáveis à loja

---
+++ src/infnet/loja/Loja.java
@@ -0,0 +1,25 @@
+package infnet.loja;
+
+import infnet.loja.*;
+import java.util.ArrayList;
+
+public class Loja 
+{
+	String endereco;
+	String nome;
+	ArrayList <Carro> estoqueDeCarros;
+	ArrayList <Motocicleta> estoqueDeMotocicletas;
+	
+	//adicionarCarro();  Adiciona um carro ao inventário, recebendo os dados do usuário
+	//pesquisarCarro(...);  Retorna um carro, utilizando os atributos passados, caso exista no inventário. 
+	//buscarCarro(Chassi); Retorna um carro, caso exista no estoque um com o Chassi indicado. 
+	//removerCarro();  Remove um carro ao inventário, recebendo os dados do usuário
+	
+	//adicionarMoto(); Adiciona uma moto ao inventário, recebendo os dados do usuário
+	//pesquisarMoto(...); Retorna um carro, utilizando os atributos passados, caso exista no inventário. 
+	//buscarMoto(Chassi); Retorna uma moto, caso exista no estoque uma com o Chassi indicado. 
+	//removerMoto();  Remove um carro ao inventário, recebendo os dados do usuário
+	
+	//listarEstoquedeMotos() 
+	//listarEstoquedeCarros()
+}
