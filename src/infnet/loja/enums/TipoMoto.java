fbe4a53667324ed8ac7bb6f3087669b13f3ceee8
Henrique Pereira <ahenriquepereira@hotmail.com>
2015-04-20 17:37:40 -0300

    Extensão alterada para .java
    Correção na Identação

--- src/infnet/loja/enums/TipoMoto.java
+++ src/infnet/loja/enums/TipoMoto.java
@@ -1,23 +1,25 @@
 package infnet.loja.enums;
 
-public enum TipoMoto {
+public enum TipoMoto 
+{
 	CHOPPER (1),
 	SCOOTER (2),
 	ESPORTIVO (3);
 	
 	private int indice;
 
-	private TipoMoto(int indice) {
+	private TipoMoto(int indice) 
+	{
 		this.indice = indice;
 	}
 
-	public int getIndice() {
+	public int getIndice() 
+	{
 		return indice;
 	}
 
-	public void setIndice(int indice) {
+	public void setIndice(int indice) 
+	{
 		this.indice = indice;
 	}
-	
-	
 }
