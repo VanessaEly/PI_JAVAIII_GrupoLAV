4ae17dab9ee57f0567ebd8df9b4a48645e699475
VanessaEly <vah_ely@hotmail.com>
2015-04-20 15:35:12 -0300

    atribuindo variáveis ao carro

---
+++ src/infnet/loja/veiculos/Carro.java
@@ -0,0 +1,13 @@
+package infnet.loja.veiculos;
+
+public class Carro 
+{
+	String chassi;
+	String montadora;
+	String modelo;
+	String tipo; 
+	String cor;
+	float motorizacao;
+	String cambio;
+	float preço;
+}
