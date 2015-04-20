3d8268a05a735cb2791ce3fba8665a96f9c410d1
Henrique Pereira <ahenriquepereira@hotmail.com>
2015-04-20 17:32:38 -0300

    Alteração na Identação

--- src/infnet/loja/enums/ModeloCarro.java
+++ src/infnet/loja/enums/ModeloCarro.java
@@ -1,21 +1,25 @@
 package infnet.loja.enums;
 
-public enum ModeloCarro {
+public enum ModeloCarro 
+{
 	FOX(1),
 	GOL(2),
 	CELTA(3);
 	
 	private int indice;
 
-	private ModeloCarro(int indice) {
+	private ModeloCarro(int indice) 
+	{
 		this.indice = indice;
 	}
 
-	public int getIndice() {
+	public int getIndice() 
+	{
 		return indice;
 	}
 
-	public void setIndice(int indice) {
+	public void setIndice(int indice) 
+	{
 		this.indice = indice;
 	}
 	
