058b163b58ddc81eabbe9a1c71066a43cafa4775
Henrique Pereira <ahenriquepereira@hotmail.com>
2015-04-22 15:11:50 -0300

    Adicionando Equals e Getters and Setters
    Add comentarios tambem

--- src/infnet/loja/veiculos/Motocicleta.java
+++ src/infnet/loja/veiculos/Motocicleta.java
@@ -1,16 +1,14 @@
-
 package infnet.loja.veiculos;
 
 import infnet.loja.enums.*;
 
 public class Motocicleta 
 {
-	/**
-	 * Classe Motocicleta define uma moto
-	 *@author Antônio, Leandro e Vanessa
-	 * 
-	**/
-	
+        /************************************** /
+	/* Classe Motocicleta define uma moto** / 
+	/*@author AntÃ´nio, Leandro e Vanessa** /
+	/***************************************/
+//-------------------------DECLARAÇÃO DE VARIAVEIS--------------------------------------------------------------------
 	String chassi;
 	Montadora montadora;
 	ModeloMoto modelo;
@@ -19,4 +17,83 @@ public class Motocicleta
 	int cilindrada;
 	int capacidadeDoTanque; 
 	float preco;
+//-------------------------EQUALS-------------------------------------------------------------------------------------
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Motocicleta other = (Motocicleta) obj;
+		if (capacidadeDoTanque != other.capacidadeDoTanque)
+			return false;
+		if (chassi == null) {
+			if (other.chassi != null)
+				return false;
+		} else if (!chassi.equals(other.chassi))
+			return false;
+		if (cilindrada != other.cilindrada)
+			return false;
+		if (cor != other.cor)
+			return false;
+		if (modelo != other.modelo)
+			return false;
+		if (montadora != other.montadora)
+			return false;
+		if (Float.floatToIntBits(preco) != Float.floatToIntBits(other.preco))
+			return false;
+		if (tipo != other.tipo)
+			return false;
+		return true;
+	}
+//-------------------------GETTERS AND SETTERS------------------------------------------------------------------------
+	public String     getChassi() {
+		return chassi;
+	}
+	public void       setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+	public Montadora  getMontadora() {
+		return montadora;
+	}
+	public void       setMontadora(Montadora montadora) {
+		this.montadora = montadora;
+	}
+	public ModeloMoto getModelo() {
+		return modelo;
+	}
+	public void       setModelo(ModeloMoto modelo) {
+		this.modelo = modelo;
+	}
+	public TipoMoto   getTipo() {
+		return tipo;
+	}
+	public void       setTipo(TipoMoto tipo) {
+		this.tipo = tipo;
+	}
+	public Cor        getCor() {
+		return cor;
+	}
+	public void       setCor(Cor cor) {
+		this.cor = cor;
+	}
+	public int        getCilindrada() {
+		return cilindrada;
+	}
+	public void       setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	public int        getCapacidadeDoTanque() {
+		return capacidadeDoTanque;
+	}
+	public void       setCapacidadeDoTanque(int capacidadeDoTanque) {
+		this.capacidadeDoTanque = capacidadeDoTanque;
+	}
+	public float      getPreco() {
+		return preco;
+	}
+	public void       setPreco(float preco) {
+		this.preco = preco;
+	}
 }
