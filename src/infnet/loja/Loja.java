d78319221c3c4ac5afebe1c683c0f0558c5e7079
Henrique Pereira <ahenriquepereira@hotmail.com>
2015-04-22 17:33:22 -0300

    Adicionando Equals e Getters and Setters em Loja

--- src/infnet/loja/Loja.java
+++ src/infnet/loja/Loja.java
@@ -21,6 +21,63 @@ public class Loja
 		this.estoqueDeCarros 	   = estoqueDeCarros;
 		this.estoqueDeMotocicletas = estoqueDeMotocicletas;
 	}
+//-------------------------EQUALS-------------------------------------------------------------------------------------
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Loja other = (Loja) obj;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (estoqueDeCarros == null) {
+			if (other.estoqueDeCarros != null)
+				return false;
+		} else if (!estoqueDeCarros.equals(other.estoqueDeCarros))
+			return false;
+		if (estoqueDeMotocicletas == null) {
+			if (other.estoqueDeMotocicletas != null)
+				return false;
+		} else if (!estoqueDeMotocicletas.equals(other.estoqueDeMotocicletas))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+//-------------------------GETTERS AND SETTERS------------------------------------------------------------------------
+	public String                 getNome() {
+		return nome;
+	}
+	public void                   setNome(String nome) {
+		this.nome = nome;
+	}
+	public String                 getEndereco() {
+		return endereco;
+	}
+	public void                   setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public ArrayList<Carro>       getEstoqueDeCarros() {
+		return estoqueDeCarros;
+	}
+	public void                   setEstoqueDeCarros(ArrayList<Carro> estoqueDeCarros) {
+		this.estoqueDeCarros = estoqueDeCarros;
+	}
+	public ArrayList<Motocicleta> getEstoqueDeMotocicletas() {
+		return estoqueDeMotocicletas;
+	}
+	public void                   setEstoqueDeMotocicletas(ArrayList<Motocicleta> estoqueDeMotocicletas) {
+		this.estoqueDeMotocicletas = estoqueDeMotocicletas;
+	}
+		
 	
 	//adicionarCarro();  Adiciona um carro ao inventário, recebendo os dados do usuário
 	//pesquisarCarro(...);  Retorna um carro, utilizando os atributos passados, caso exista no inventário. 
