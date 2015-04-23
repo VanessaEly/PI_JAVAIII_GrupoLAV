80b65de16743b57c246b8d5a285b624d7c78ece7
Vanessa Carla Ely <vah_ely@hotmail.com>
2015-04-23 20:04:39 -0300

    Alterações na classe Principal

--- src/infnet/Principal.java
+++ src/infnet/Principal.java
@@ -13,13 +13,11 @@ public class Principal
 		Loja loja1 = new Loja("Lojas de Teste", "Rua Qualquer, nÂº 2");
 		System.out.println("Loja: " + loja1.getNome() + "\n End: " + loja1.getEndereco() + "\n" );		
 //-------------------------Adicionando Carros para Teste---------------------------------------------------------------------------------------------		
-		Carro carro1 = new Carro(null, null, null, null, null, 0, null, 0);
-		loja1.adicionarCarro(carro1);
+		loja1.adicionarCarro();
 		loja1.listarEstoqueDeCarros();
 		System.out.println();
 //-------------------------Adicionando Motos para Teste----------------------------------------------------------------------------------------------		
-		Motocicleta moto1= new Motocicleta(null, null, null, null, null, 0, 0, 0);
-		loja1.adicionarMoto(moto1);
+		loja1.adicionarMoto();
 		loja1.listarEstoqueDeMotocicletas();
 //-------------------------Fim do Main---------------------------------------------------------------------------------------------------------------		
 	}
