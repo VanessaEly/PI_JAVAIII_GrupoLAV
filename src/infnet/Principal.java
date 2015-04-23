0443b8d8c9753e47f51380bdc06a96ba353011e9
Henrique Pereira <ahenriquepereira@hotmail.com>
2015-04-23 18:20:08 -0300

    Teste dos metodos inputados pelo usuario

--- src/infnet/Principal.java
+++ src/infnet/Principal.java
@@ -10,20 +10,16 @@ public class Principal
 	public static void main(String[] args) 
 	{
 //-------------------------Adicionando Loja para Teste---------------------------------------------------------------------------------------------
-		Loja loja1 = new Loja("Lojas de Teste", "Rua Qualquer, nº 2");
+		Loja loja1 = new Loja("Lojas de Teste", "Rua Qualquer, nÂº 2");
 		System.out.println("Loja: " + loja1.getNome() + "\n End: " + loja1.getEndereco() + "\n" );		
 //-------------------------Adicionando Carros para Teste---------------------------------------------------------------------------------------------		
-		Carro carro1 = new Carro("EOF31231", Montadora.CHEVROLET, ModeloCarro.CELTA, TipoCarro.SEDAN, Cor.AMARELO, 1.6f, Cambio.AUTO, 25000.350f);
-		Carro carro2 = new Carro("AAJ13123", Montadora.FIAT, ModeloCarro.GOL, TipoCarro.SUV, Cor.BRANCO, 1.6f, Cambio.SEMI, 40200.00f);
+		Carro carro1 = new Carro(null, null, null, null, null, 0, null, 0);
 		loja1.adicionarCarro(carro1);
-		loja1.adicionarCarro(carro2);
 		loja1.listarEstoqueDeCarros();
 		System.out.println();
 //-------------------------Adicionando Motos para Teste----------------------------------------------------------------------------------------------		
-		Motocicleta moto1= new Motocicleta ("JDF96598", Montadora.HONDA, ModeloMoto.CBR, TipoMoto.ESPORTIVO, Cor.AZUL, 100, 150, 15000.350f);
-		Motocicleta moto2= new Motocicleta ("PGI94652", Montadora.KAWASAKI, ModeloMoto.NINJA, TipoMoto.ESPORTIVO, Cor.BRANCO, 120, 180, 25000.150f);
-		loja1.adicionarMotocicletas(moto1);
-		loja1.adicionarMotocicletas(moto2);
+		Motocicleta moto1= new Motocicleta(null, null, null, null, null, 0, 0, 0);
+		loja1.adicionarMoto(moto1);
 		loja1.listarEstoqueDeMotocicletas();
 //-------------------------Fim do Main---------------------------------------------------------------------------------------------------------------		
 	}
